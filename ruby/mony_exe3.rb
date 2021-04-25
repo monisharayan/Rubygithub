edc8b5da0a69129c6d4235869cd67754efc66f14
monisharayan <82630533+monisharayan@users.noreply.github.com>
2021-04-25 14:42:00 +0530

    Ruby Programs

---
+++ ruby/mony_exe3.rb
@@ -0,0 +1,14 @@
+puts "I will now count my chickens:"
+puts "Hens#{25+10/2}"
+puts "Roosters #{10-25*3%5}"
+puts "Now I will count the eggs:"
+puts 3+2+1-5+7%4-1/4+6
+puts "Is it true that 5+5<2-1"
+puts 5+5<2-1
+puts "What is 3+2? #{3+2}"
+puts "What is 5-2? #{5-2}"
+puts "Oh, that's why it's false."
+puts "How about some more."
+puts "Is it greater? #{5>-3}"
+puts "Is it greater or equal? #{5>=3}"
+puts "Is it less or equal? #{5<=-2}"
---
+++ ruby/mony_exe5.rb
@@ -0,0 +1,16 @@
+my_name = 'Monisha Rayan'
+my_age = 21
+my_height = 170
+my_weight = 60
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
